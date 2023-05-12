13f1f70b6b890383152933263ca42f7add000d44
Kevin Huck <khuck@cs.uoregon.edu>
2023-05-12 13:02:33 -0400

    Adding install support

--- frontier.sh
+++ frontier.sh
@@ -4,9 +4,11 @@ rm -rf build
 
 cmake -B build \
 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+-DCMAKE_INSTALL_PREFIX=`pwd`/install \
 -DZeroSum_STANDALONE=TRUE \
 
 cmake --build build -j
+cmake --install build
 #ctest --test-dir build
 
 #-DZeroSum_WITH_PerfStubs=FALSE \
