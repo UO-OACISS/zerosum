5f6fa6a9d7e602e35f8d0617887d7f519178c1b1
Kevin Huck <khuck@cs.uoregon.edu>
2023-05-22 09:30:20 -0700

    Somehow deleted a chunk of code, restoring that.

--- src/zerosum_pthreads.cpp
+++ src/zerosum_pthreads.cpp
@@ -39,7 +39,22 @@ int ZeroSum::getpthreads() {
         while ((ep = readdir (dp)) != NULL) {
             if (strncmp(ep->d_name, ".", 1) == 0) continue;
             if (tmpstr.size() > 0) { tmpstr = tmpstr + ","; }
-//fields.insert(std::pair("step",std::to_string(step)));
+            tmpstr = tmpstr + ep->d_name;
+            uint32_t lwp = atol(ep->d_name);
+            int nhwthr = 0;
+            std::string tmpstr2;
+            std::vector<uint32_t> allowed_list =
+                getAffinityList(lwp, computeNode.ncpus, nhwthr, tmpstr2);
+            // also want to read /proc/<pid>/task/<tid>/status!
+            std::string filename = "/proc/self/task/";
+            filename += ep->d_name;
+            filename += "/stat";
+            //std::string allowed_string = getCpusAllowed(filename.c_str());
+            auto fields = getThreadStat(filename.c_str());
+            filename += "us";
+            getThreadStatus(filename.c_str(), fields);
+            //std::cout << filename << " : " << allowed_string << std::endl;
+            //fields.insert(std::pair("step",std::to_string(step)));
             if (lwp == async_tid) {
                 this->process.add(lwp, allowed_list, fields, software::ThreadType::ZeroSum);
             } else {
