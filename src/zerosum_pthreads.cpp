372e073aac2f7a00138ffb92b665f89502edc6a1
Kevin Huck <khuck@cs.uoregon.edu>
2023-05-22 09:02:40 -0700

    Updating license message everywhere

--- src/zerosum_pthreads.cpp
+++ src/zerosum_pthreads.cpp
@@ -1,8 +1,26 @@
-/**********************************************************
-  "Hello World"-type program to test different srun layouts.
-
-  Written by Tom Papatheodore
- **********************************************************/
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 University of Oregon, Kevin Huck
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #include <string.h>
 #include <dirent.h>
@@ -21,22 +39,7 @@ int ZeroSum::getpthreads() {
         while ((ep = readdir (dp)) != NULL) {
             if (strncmp(ep->d_name, ".", 1) == 0) continue;
             if (tmpstr.size() > 0) { tmpstr = tmpstr + ","; }
-            tmpstr = tmpstr + ep->d_name;
-            uint32_t lwp = atol(ep->d_name);
-            int nhwthr = 0;
-            std::string tmpstr2;
-            std::vector<uint32_t> allowed_list =
-                getAffinityList(lwp, computeNode.ncpus, nhwthr, tmpstr2);
-            // also want to read /proc/<pid>/task/<tid>/status!
-            std::string filename = "/proc/self/task/";
-            filename += ep->d_name;
-            filename += "/stat";
-            //std::string allowed_string = getCpusAllowed(filename.c_str());
-            auto fields = getThreadStat(filename.c_str());
-            filename += "us";
-            getThreadStatus(filename.c_str(), fields);
-            //std::cout << filename << " : " << allowed_string << std::endl;
-            //fields.insert(std::pair("step",std::to_string(step)));
+//fields.insert(std::pair("step",std::to_string(step)));
             if (lwp == async_tid) {
                 this->process.add(lwp, allowed_list, fields, software::ThreadType::ZeroSum);
             } else {
