3e03edc2e91989ca0df9e5a07faf6e164d4b9afe
Kevin Huck <khuck@cs.uoregon.edu>
2023-06-14 16:34:06 +0000

    Adding SYCL GPU static features

--- src/zerosum_sycl.cpp
+++ src/zerosum_sycl.cpp
@@ -50,11 +50,61 @@ int ZeroSum::getgpu(void) {
             std::cout << "(root-dev) TotalMem (bytes): " << totalMemory << ", FreeMem (bytes): " << freeMemory << std::endl;
         }
         fields.insert(std::pair(std::string("Name"), d.get_info<sycl::info::device::name>()));
+        fields.insert(std::pair(std::string("Vendor"), d.get_info<sycl::info::device::vendor>()));
+        fields.insert(std::pair(std::string("Driver Version"), d.get_info<sycl::info::device::driver_version>()));
+        fields.insert(std::pair(std::string("Version"), d.get_info<sycl::info::device::version>()));
+        //fields.insert(std::pair(std::string("Backend Version"), d.get_info<sycl::info::device::backend_version>()));
         fields.insert(std::pair(std::string("RT_GPU_ID"), std::to_string(index++)));
         fields.insert(std::pair(std::string("TotalMem (bytes)"),
             std::to_string(totalMemory)));
         fields.insert(std::pair(std::string("FreeMem (bytes)"),
             std::to_string(freeMemory)));
+        fields.insert(std::pair(std::string("Max Compute Units"),
+            std::to_string(d.get_info<sycl::info::device::max_compute_units>())));
+        fields.insert(std::pair(std::string("Max Work Item Dimensions"),
+            std::to_string(d.get_info<sycl::info::device::max_work_item_dimensions>())));
+        fields.insert(std::pair(std::string("Max Work Group Size"),
+            std::to_string(d.get_info<sycl::info::device::max_work_group_size>())));
+        fields.insert(std::pair(std::string("Max Clock Frequency (MHz)"),
+            std::to_string(d.get_info<sycl::info::device::max_clock_frequency>())));
+        fields.insert(std::pair(std::string("Max Memory Allocation Size (B)"),
+            std::to_string(d.get_info<sycl::info::device::max_mem_alloc_size>())));
+        fields.insert(std::pair(std::string("Global Memory Cache Line Size (B)"),
+            std::to_string(d.get_info<sycl::info::device::global_mem_cache_line_size>())));
+        fields.insert(std::pair(std::string("Global Memory Cache Size (B)"),
+            std::to_string(d.get_info<sycl::info::device::global_mem_cache_size>())));
+        fields.insert(std::pair(std::string("Global Memory Size (B)"),
+            std::to_string(d.get_info<sycl::info::device::global_mem_size>())));
+        fields.insert(std::pair(std::string("Local Memory Size (B)"),
+            std::to_string(d.get_info<sycl::info::device::local_mem_size>())));
+        /* Get some intel-specific features */
+        fields.insert(std::pair(std::string("Device ID"),
+            std::to_string(d.get_info<sycl::ext::intel::info::device::device_id>())));
+	/* Crashes on sunspot? */
+        //fields.insert(std::pair(std::string("PCI Address"), d.get_info<sycl::ext::intel::info::device::pci_address>()));
+        fields.insert(std::pair(std::string("EU Count"),
+            std::to_string(d.get_info<sycl::ext::intel::info::device::gpu_eu_count>())));
+        fields.insert(std::pair(std::string("EU SIMD Width"),
+            std::to_string(d.get_info<sycl::ext::intel::info::device::gpu_eu_simd_width>())));
+        fields.insert(std::pair(std::string("Slices"),
+            std::to_string(d.get_info<sycl::ext::intel::info::device::gpu_slices>())));
+        fields.insert(std::pair(std::string("Subslices per Slice"),
+            std::to_string(d.get_info<sycl::ext::intel::info::device::gpu_subslices_per_slice>())));
+        fields.insert(std::pair(std::string("EU Count per Subslice"),
+            std::to_string(d.get_info<sycl::ext::intel::info::device::gpu_eu_count_per_subslice>())));
+        fields.insert(std::pair(std::string("HW Threads per EU"),
+            std::to_string(d.get_info<sycl::ext::intel::info::device::gpu_hw_threads_per_eu>())));
+	/* Crashes on sunspot? */
+        //fields.insert(std::pair(std::string("Max Memory Bandwidth"),
+            //std::to_string(d.get_info<sycl::ext::intel::info::device::max_mem_bandwidth>())));
+        //fields.insert(std::pair(std::string("UUID"),
+            //std::to_string(d.get_info<sycl::ext::intel::info::device::uuid>())));
+        fields.insert(std::pair(std::string("Memory Clock Rate"),
+            std::to_string(d.get_info<sycl::ext::intel::info::device::memory_clock_rate>())));
+        fields.insert(std::pair(std::string("Memory Bus Width"),
+            std::to_string(d.get_info<sycl::ext::intel::info::device::memory_bus_width>())));
+        fields.insert(std::pair(std::string("Max Compute Queue Indices"),
+            std::to_string(d.get_info<sycl::ext::intel::info::device::max_compute_queue_indices>())));
 
         allfields.push_back(fields);
     }
