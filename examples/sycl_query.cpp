9c839bb603ceb1fc47419bd79f9daf7c7c4268e0
Kevin Huck <khuck@cs.uoregon.edu>
2023-06-15 11:49:22 -0700

    Adding try/catch around sycl queries

--- examples/sycl_query.cpp
+++ examples/sycl_query.cpp
@@ -12,32 +12,42 @@ int main() {
         /* go through sycl */
         std::cout << "SYCL GPU-ID: " << d.get_info<sycl::info::device::name>() << std::endl;
         size_t totalMemory = d.get_info<sycl::info::device::global_mem_size>();
-        size_t freeMemory = d.get_info<sycl::ext::intel::info::device::free_memory>();
+        size_t freeMemory = totalMemory;
+        try {
+            freeMemory = d.get_info<sycl::ext::intel::info::device::free_memory>();
+        } catch (...) {
+            std::cerr << "Error reading intel device free memory." << std::endl;
+        }
         std::cout << "SYCL TotalMem (GB): " << totalMemory / std::pow(1024, 3)
             << ", FreeMem (GB): " << freeMemory / std::pow(1024, 3) << std::endl;
 
         /* go through level0 */
         ze_result_t status = ZE_RESULT_SUCCESS;
-        auto ze_dev = ::sycl::get_native<::sycl::backend::ext_oneapi_level_zero>(d);
-        uint32_t module_count = 0;
-        status = zesDeviceEnumMemoryModules(ze_dev, &module_count, nullptr);
-        if( module_count > 0 ) {
-            std::vector<zes_mem_handle_t> module_list(module_count);
-            std::vector<zes_mem_state_t> state_list(module_count);
+        try {
+            auto ze_dev = ::sycl::get_native<::sycl::backend::ext_oneapi_level_zero>(d);
+            uint32_t module_count = 0;
+            status = zesDeviceEnumMemoryModules(ze_dev, &module_count, nullptr);
+            if( module_count > 0 ) {
+                std::vector<zes_mem_handle_t> module_list(module_count);
+                std::vector<zes_mem_state_t> state_list(module_count);
 
-            status = zesDeviceEnumMemoryModules(ze_dev, &module_count, module_list.data());
-            if (status == ZE_RESULT_SUCCESS) {
-                for (uint32_t i = 0; i < module_count; ++i) {
-                    status = zesMemoryGetState(module_list[i], &(state_list[i]));
-                    std::string prefix{"Level0 Module "};
-                    prefix += std::to_string(i);
-                    std::cout << prefix << " TotalMem (GB): "
-                        << state_list[i].size / std::pow(1024, 3)
-                        << ", FreeMem (GB): "
-                        << state_list[i].free / std::pow(1024, 3) << std::endl;
+                status = zesDeviceEnumMemoryModules(ze_dev, &module_count, module_list.data());
+                if (status == ZE_RESULT_SUCCESS) {
+                    for (uint32_t i = 0; i < module_count; ++i) {
+                        status = zesMemoryGetState(module_list[i], &(state_list[i]));
+                        std::string prefix{"Level0 Module "};
+                        prefix += std::to_string(i);
+                        std::cout << prefix << " TotalMem (GB): "
+                            << state_list[i].size / std::pow(1024, 3)
+                            << ", FreeMem (GB): "
+                            << state_list[i].free / std::pow(1024, 3) << std::endl;
+                    }
                 }
             }
+        } catch (...) {
+            std::cerr << "Error getting native intel device." << std::endl;
         }
+        std::cout << std::endl;
     }
     return 0;
 }
