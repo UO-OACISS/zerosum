95056ae3ad1e43e977edef83e8538a9a21ea06c1
Kevin Huck <khuck@cs.uoregon.edu>
2023-12-01 09:41:01 -0500

    updating job script

--- scripts/job-frontier.sh
+++ scripts/job-frontier.sh
@@ -8,15 +8,19 @@
 #SBATCH -S 8
 
 # Assumption - this script will reserve 1 core per L3 bank for system activity.
+#module load rocm/5.2.0
+cd $HOME/ECP-WDM/ecp-wdm-coe
+source frontier.sh
+cd $HOME/src/zerosum
 
-#export OMP_PROC_BIND=spread
-#export OMP_PLACES=cores
-#export OMP_NUM_THREADS=14
-#export OMP_SCHEDULE=guided
+unset OMP_PROC_BIND
+unset OMP_PLACES
+unset OMP_NUM_THREADS
+unset OMP_SCHEDULE
 export SLURM_CPU_BIND_VERBOSE=1
 
 dosrun() {
-    srun -n8 --gpus-per-task=1 --cpus-per-task=14 --gpu-bind=closest ./build.5.4.3/bin/zerosum-mpi ./build.5.4.3/bin/lu-decomp-mpi
+    srun -n8 --gpus-per-task=1 --cpus-per-task=14 --gpu-bind=closest ./build.5.2.0/bin/zerosum-mpi ./build.5.2.0/bin/lu-decomp-mpi
 }
 
 echo "Defaults"
@@ -29,6 +33,7 @@ echo "With Cores"
 export OMP_PROC_BIND=close
 export OMP_PLACES=cores
 export OMP_NUM_THREADS=14
+export OMP_SCHEDULE=dynamic
 dosrun
 for i in {0..7} ; do
     mv zs.${i}.log zs.14.cores.${i}.log
