e61fdfd8a8fba52a19f9a3d23004dc6d70785cef
Kevin Huck <khuck@cs.uoregon.edu>
2025-01-30 13:55:26 -0500

    Cleaning up cray power measurements

--- src/cray_pm_counters.cpp
+++ src/cray_pm_counters.cpp
@@ -18,7 +18,46 @@ using namespace std;
 
 namespace zerosum {
 
-cray_pm_counters::cray_pm_counters() : supported(false)
+uint64_t cray_pm_counters::get_unitless(std::string name) {
+    uint64_t tmpint{0};
+    std::string filename = location + name;
+    FILE * fp = fopen(filename.c_str(), "r");
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    while ((nread = getline(&line, &len, fp)) != -1) {
+        if (nread == -1) break;
+        std::istringstream iline{line};
+        while (iline >> tmpint) {
+            return tmpint;
+            break;
+        }
+    }
+    fclose(fp);
+    return tmpint;
+}
+
+std::pair<uint64_t,std::string> cray_pm_counters::get_with_unit(std::string name) {
+    uint64_t tmpint{0};
+    std::string tmpstr;
+    std::string filename = location + name;
+    FILE * fp = fopen(filename.c_str(), "r");
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    while ((nread = getline(&line, &len, fp)) != -1) {
+        if (nread == -1) break;
+        std::istringstream iline{line};
+        while (iline >> tmpint >> tmpstr) {
+            break;
+        }
+    }
+    fclose(fp);
+    return std::make_pair(tmpint, tmpstr);
+}
+
+cray_pm_counters::cray_pm_counters() : supported(false),
+    previous_generation(0), previous_freshness(0)
 {
     struct dirent *entry = nullptr;
     DIR * directory_ptr = opendir(location.c_str());
@@ -32,32 +71,20 @@ cray_pm_counters::cray_pm_counters() : supported(false)
         if (strstr(entry->d_name,"_cap") != 0) continue;
         if (skip.count(name) != 0) continue;
         // read the initial value, save units and full name
-        uint64_t tmpint;
-        std::string tmpstr;
-        std::string filename = location + name;
-        FILE * fp = fopen(filename.c_str(), "r");
-        if (fp != NULL) {
-            char *line = NULL;
-            size_t len = 0;
-            ssize_t nread;
-            while ((nread = getline(&line, &len, fp)) != -1) {
-                if (nread == -1) break;
-                std::istringstream iline{line};
-                while (iline >> tmpint >> tmpstr) {
-                    bool monotonic{false};
-                    std::string fullname = name + std::string(" (") +
-                        tmpstr + std::string(")");
-                    if (tmpstr.compare("J") == 0) { monotonic = true; }
-                    previous.insert(
-                        std::pair<std::string,cray_tuple>(
-                            name, std::make_tuple(
-                                fullname, tmpint, monotonic)));
-                    break;
-                }
-            }
-        }
-        fclose(fp);
+        auto data = get_with_unit(name);
+        auto tmpint = std::get<uint64_t>(data);
+        auto tmpstr = std::get<std::string>(data);
+        bool monotonic{false};
+        std::string fullname = std::string("cray_pm ") + name +
+            std::string(" (") + tmpstr + std::string(")");
+        if (tmpstr.compare("J") == 0) { monotonic = true; }
+        previous.insert(
+            std::pair<std::string,cray_tuple>(
+                name, std::make_tuple(
+                    fullname, tmpint, monotonic)));
     }
+    previous_freshness = get_unitless("freshness");
+    previous_generation = get_unitless("generation");
     supported = true;
     closedir(directory_ptr);
 }
@@ -70,6 +97,23 @@ std::map<std::string,std::string> cray_pm_counters::read_counters()
 {
     std::map<std::string,std::string> fields;
     if (!supported) return fields;
+
+    // confirm we have good values
+    uint64_t new_freshness = get_unitless("freshness");
+    //fields.insert( std::pair<std::string,std::string>(
+        //"freshness", std::to_string(new_freshness)));
+    bool valid{new_freshness > previous_freshness};
+    fields.insert( std::pair<std::string,std::string>(
+        "cray_pm valid", std::to_string(valid)));
+    previous_freshness = new_freshness;
+
+    // confirm we haven't changed power caps
+    uint64_t new_generation = get_unitless("generation");
+    bool changed{new_generation > previous_generation};
+    fields.insert( std::pair<std::string,std::string>(
+        "cray_pm power cap changed", std::to_string(changed)));
+    previous_generation = new_generation;
+
     // iterate over the tuples, with a reference because we want to update them
     for (auto& p : previous) {
         std::string fullname;
@@ -77,31 +121,16 @@ std::map<std::string,std::string> cray_pm_counters::read_counters()
         bool monotonic;
         // tie will give us references to these values...
         std::tie(fullname, prev, monotonic) = p.second;
-        uint64_t tmpint;
-        std::string tmpstr;
-        std::string filename = location + p.first;
-        FILE * fp = fopen(filename.c_str(), "r");
-        if (fp != NULL) {
-            char *line = NULL;
-            size_t len = 0;
-            ssize_t nread;
-            while ((nread = getline(&line, &len, fp)) != -1) {
-                if (nread == -1) break;
-                std::istringstream iline{line};
-                while (iline >> tmpint >> tmpstr) {
-                    auto current = tmpint;
-                    if (monotonic) {
-                        current = current - prev;
-                        std::get<1>(p.second) = tmpint;
-                    }
-                    fields.insert(
-                        std::pair<std::string,std::string>(
-                            fullname, std::to_string(current)));
-                    break;
-                }
-            }
+        auto data = get_with_unit(p.first);
+        auto tmpint = std::get<uint64_t>(data);
+        auto current = tmpint;
+        if (monotonic) {
+            current = current - prev;
+            std::get<1>(p.second) = tmpint;
         }
-        fclose(fp);
+        fields.insert(
+            std::pair<std::string,std::string>(
+                fullname, std::to_string(current)));
     }
     return fields;
 }
