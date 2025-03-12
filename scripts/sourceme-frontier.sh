191e0262bf951197f39ffca87cd72750777bbbfb
Kevin Huck <khuck@cs.uoregon.edu>
2025-03-12 17:21:55 -0400

    minor frontier version change

--- scripts/sourceme-frontier.sh
+++ scripts/sourceme-frontier.sh
@@ -2,8 +2,8 @@ module reset
 module unload perftools-base
 module load cmake
 module load PrgEnv-amd
-module swap amd amd/5.4.3
-module load rocm/5.4.3
+module swap amd amd/5.7.1
+module load rocm/5.7.1
 module swap cray-mpich cray-mpich/8.1.28
 module load craype-accel-amd-gfx90a
 module unload darshan-runtime
