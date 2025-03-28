0d41874f9371171a64cdd4981cd37176e3fbfab7
Kevin Huck <khuck@cs.uoregon.edu>
2025-03-28 14:33:56 -0700

    Forcing git submodule init to fix CI

--- cmake/AddGitSubmodule.cmake
+++ cmake/AddGitSubmodule.cmake
@@ -35,11 +35,11 @@ function(add_git_submodule dir)
 
 find_package(Git REQUIRED)
 
-if(NOT EXISTS ${dir}/CMakeLists.txt)
+#if(NOT EXISTS ${dir}/CMakeLists.txt)
   execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     COMMAND_ERROR_IS_FATAL ANY)
-endif()
+#endif()
 
 add_subdirectory(${dir})
 
